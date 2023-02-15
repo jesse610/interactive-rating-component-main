9a612d52f91fc4682fdb4ced07345cd8801d7965
Jesse <jesse.velasquez40@gmail.com>
2023-02-15 13:48:31 -0800

    added eventlistener to form

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
-let numBtns = document.querySelectorAll('.number')
+let form = document.querySelector('#form')
+let ratings = document.getElementsByName('rating')
+let numLabel = document.querySelectorAll('.number-label')
 let submitBtn = document.querySelector('#submit-btn')
 let ratingContainer = document.querySelector('.rating-container')
 let thankYouContainer = document.querySelector('.thank-you-container')
 let selection = document.querySelector('#selection')
 let error = document.querySelector('#error')
-let numValue = 0;
 
-numBtns.forEach(btn => {
-    btn.addEventListener('click', getNum)
-    btn.addEventListener('click', checkActive)
-    btn.addEventListener('click', function() {
-        this.classList.add('active')
+numLabel.forEach(label => {
+    label.addEventListener('click', checkActive)
+    label.addEventListener('click', function() {
+        label.classList.add('active')
     })
 })
 
-submitBtn.addEventListener('click', changeSection)
-
-function changeSection() {
-    if (numValue === 0) {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let rating = form.rating.value;
+    if (rating === '') {
         error.textContent = 'Error! Please make a selection.'
     } else {
+        selection.textContent = `You selected ${rating} out of 5`;
         ratingContainer.classList.add('remove')
         thankYouContainer.classList.remove('remove')
-        selection.textContent = `You selected ${numValue} out of 5`
     }
-}
+})
 
-function getNum() {
-    numValue = this.value
-
-}
 
 function checkActive() {
     for (let i = 0; i < 5; i++) {
-        if (numBtns[i].classList[1] == 'active') {
-            numBtns[i].classList.remove('active')
+        if (numLabel[i].classList[1] == 'active') {
+            numLabel[i].classList.remove('active')
         }
     }
 }
